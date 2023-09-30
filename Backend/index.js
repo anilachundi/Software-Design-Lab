167842e665067d8d9428dddadd9e8305809dee61
Jericho Calago <jcalago02@gmail.com>
2023-09-30 12:47:37 -0500

    Created basic 'hello world' GET route

--- Backend/index.js
+++ Backend/index.js
@@ -5,3 +5,9 @@ app.listen(
     PORT,
     () => console.log(`alive on http://localhost:${PORT}`)
 )
+
+// GET
+// http://localhost:8080/testRoute
+app.get('/testRoute', async (req, res) => { // test get request to return "hello world" as a message in JSON
+    res.status(200).send({message: "Hello world"});
+})
