ca149ab65025be22fbbb6f926ba5485917c53645
Jericho Calago <jcalago02@gmail.com>
2023-10-06 13:55:34 -0500

    add function to connect to mongodb database

--- Backend/index.js
+++ Backend/index.js
@@ -1,13 +1,44 @@
 const app = require('express')();
-const PORT = 8080;
+const connectFunction = require('./database');
 
 app.listen(
-    PORT,
-    () => console.log(`alive on http://localhost:${PORT}`)
+    process.env.PORT,
+    () => console.log(`alive on http://localhost:${process.env.PORT}`)
 )
+
+
+
+// middleware to connect to mongodb client
+app.use(async (req, res, next) => {
+    console.log("route called");
+    try {
+        // connect to database via function in database.js
+        const client = await connectFunction();
+
+        // grab db object from client and pass to route via req object
+        const db = client.db('CS222Backend');
+        req.db = db;
+
+        // proceed to expressjs route
+        console.log("connected to db");
+        next();
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({message: 'Database connection error',
+                              error : error});
+    }
+})
+
 
 // GET
 // http://localhost:8080/testRoute
 app.get('/testRoute', async (req, res) => { // test get request to return "hello world" as a message in JSON
     res.status(200).send({message: "Hello world"});
+
+    name = req.body.name
+
 })
+
+
+
+
