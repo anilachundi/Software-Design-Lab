8cf3562c53b4d50223bc48619521ebf855312328
anilachundi <achundi2@illinois.edu>
2023-10-06 14:05:39 -0500

    Updating Welcome.js

---
+++ Frontend/pages/Calendar.js
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import RootStack from './../naviC:\Users\anila\.vscode\group-project-team116\Frontend\navigators\RootStack.jsgators/RootStack'; //React navigation stackr
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+});
+  
+const Calendar = ({ navigation }) => {
+    <Text>calendar!</Text>
+}
